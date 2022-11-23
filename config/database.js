b63d93de293d79698ef6e4ea28379556900aaaac
CSH <sayong4424@gmail.com>
2022-11-23 19:47:31 +0900

    221123 : Db 연결 및 jwt secret key 입력

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
+    host: 'database-1.clf9mchvf7gx.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
     port: '3306',
-    password: '',
-    database: ''
+    password: 'gagofarm1719',
+    database: 'gago_farm'
 });
 
 module.exports = {
